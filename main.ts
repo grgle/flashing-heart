cdf827177b9aed687c13425809ab9e473dae3e3a
grgle <62070601+grgle@users.noreply.github.com>
2022-11-26 02:34:16 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+bluetooth.onBluetoothConnected(function () {
+    bluetooth.startButtonService()
+})
+input.onButtonPressed(Button.A, function () {
+    min += 100
+})
+input.onButtonPressed(Button.AB, function () {
+    max = 1023
+    min = 0
+})
+input.onButtonPressed(Button.B, function () {
+    max += -100
+})
+let max = 0
+let min = 0
+let power = 0
+min = 0
+max = 1023
+basic.forever(function () {
+    for (let list = 0; list <= max; list++) {
+        power += 1
+        pins.analogWritePin(AnalogPin.P0, power)
+        pins.analogWritePin(AnalogPin.P1, power)
+        pins.analogWritePin(AnalogPin.P2, power)
+        basic.pause(1)
+        if (power >= max) {
+            list = 0
+            for (let list = 0; list <= 1023; list++) {
+                power += -1
+                pins.analogWritePin(AnalogPin.P0, power)
+                pins.analogWritePin(AnalogPin.P1, power)
+                pins.analogWritePin(AnalogPin.P2, power)
+                basic.pause(1)
+                if (power <= min) {
+                    break;
+                }
+            }
+        }
+    }
+})
